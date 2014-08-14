0e16e0946a50df78c92c4e275967e98c2813107a
Jiří Roubalík <jiriroubalik@gmail.com>
2014-08-15 00:41:09 +0200

    translate miband localization_czech.lua

--- Czech/main/MiBand.apk/res/raw-sk/localization_czech.lua
+++ Czech/main/MiBand.apk/res/raw-sk/localization_czech.lua
@@ -1,9 +1,9 @@
 -------------------------
--- Slovak Localization String resources
+-- Czech Localization String resources
 -- miui.cz (2014)
--- ingbzry
+-- kynio
 -------------------------
-localization_Slovak_table = {
+localization_czech_table = {
     calories_table={
 
     },
@@ -12,57 +12,57 @@ localization_Slovak_table = {
 
     hour='hod',
     minute='min',
-    month='mesiac',
-    day='deň',
+    month='měsíc',
+    day='den',
     step="krok",
     km='KM',
     minute_format='%d min',
     hour_minute_format='%dh %dm',
     hour_format='%d h',
 
-    welcome_use="Vitajte, ja som váš MiBand.",
-    last_week_walked_format='Minulý týždeň ste prešli %d krokov.',
-    last_week_walked_info_format='Prešli ste %d km a spálili %d kcal.',
-    last_month_walked_format='Minulý mesiac ste prešli %d krokov.',
-    last_month_walked_info_format='Prešli ste %d km a spálili %d kcal.',
+    welcome_use="Vítejte, já jsem váš MiBand.",
+    last_week_walked_format='Minulý týden jste ušli %d kroků.',
+    last_week_walked_info_format='Ušli jste %d km a spálili %d kcal.',
+    last_month_walked_format='Minulý měsíc jste ušli %d kroků.',
+    last_month_walked_info_format='Ušli jste %d km a spálili %d kcal.',
 
-    get_over_format=", okolo %d gramov tuku.",
-    click_to_get_help="Pozrite sa čo dokáže MiBand.",
-    take_a_walk="Prejdite sa s MiBand.",
-    take_a_walk_info="MiBand bude zaznamenávať vašu aktivitu a pomôže vám byť zdravý.",
-    unlock_hint="Odomknite váš telefón s MiBand,",
-    unlock_hint_info="objavte nový spôsob ochrany vášho súkromia.",
-    no_data_hint_0="Hýbte sa a športujte pre vaše zdravie.",
-    no_data_hint_1="Chcete byť stále šťastný? Zabehajte si.",
-    no_data_hint_2="Kardio cvičenie vás osvieži a rozhýbe každý deň! ",
-    no_data_hint_3="Bežný dospelý človek strávi 15%-20% spánku v hlbokej fáze. ",
-    not_binded_hint="Nemáte prepojený MiBand s týmto účtom, prepojte ho teraz. ",
-    new_record_info="WoW! Práve ste zdolali rekord v chôdzi.",
-    new_record_format="Nový rekord, %d krokov, %s %d",
-    today_goal_reached="Gratulujeme! Dosiahli ste váš dnešný cieľ. ",
-    week_continue_reach_goal="Splnili ste váš denný cieľ po celý týždeň! To bol skvelý týždeň!",
-    challenge_format="Dosiahli ste váš cieľ pre %d dní!",
-    personal_best_format="Váš najväčší rekord je %d dní!",
+    get_over_format=", kolem %dg tuku.",
+    click_to_get_help="Mrkněte co dokáže MiBand.",
+    take_a_walk="Projděte se s MiBand.",
+    take_a_walk_info="MiBand bude zaznamenávat vaší aktivitu a pomůže vám být zdravější.",
+    unlock_hint="Odemkněte váš telefon s MiBand,",
+    unlock_hint_info="Objevte nový způsob ochrany vašeho soukromí.",
+    no_data_hint_0="Hýbejte se a sportujte pro vaše zdraví.",
+    no_data_hint_1="Chcete být stále šťastní? Zaběhejte si.",
+    no_data_hint_2="Kardio cvičení vás osvěží a rozhýbe každý den! ",
+    no_data_hint_3="Běžný dospělý člověk stráví 15%-20% spánku v hluboké fázi. ",
+    not_binded_hint="Nemáte propojený MiBand s tímto účtem, propojte jej nyní. ",
+    new_record_info="WoW! Právě byl zdolán rekord v chůzi.",
+    new_record_format="Nový rekord, %d kroků, %s %d",
+    today_goal_reached="Gratulujeme! Byl dosažen váš dnešní cíl. ",
+    week_continue_reach_goal="Za poslední týden byl každý den splněn váš cíl! Byl to skvělý týden!",
+    challenge_format="Byl dosažen váš cíl pro %d dní!",
+    personal_best_format="Váš nejvyšší rekord je %d dní!",
     active_time_format_0="%dh",
     active_time_format_1="%dh",
     active_time_format_2="%dh %dmin",
-    activie_run_format_0="Zabehli ste %s. Skvelé!",
-    activie_run_format_1="Zabehli ste %s. Len tak ďalej!",
-    activie_run_format_2="Zabehli ste %s, Neskutočné!",
-    activie_run_consumed="Spálili ste %d kcal. %s",
+    activie_run_format_0="Uběhli jste %s. Skvělé!",
+    activie_run_format_1="Uběhli jste %s. Jen tak dál!",
+    activie_run_format_2="Uběhli jste %s, Neskutečné!",
+    activie_run_consumed="Uběhli jste %d kcal. %s",
 
-    get_distance_format=", celkovo z %dm.",
-    activity_walk_format="%s Chôdza %d krokov %s",
+    get_distance_format=", celkem z %dm.",
+    activity_walk_format="%s Chůze %d kroků %s",
     activity_walk_consumed_format="Spálené %d kcal %s",
 
-    activity_activity_format="%s Urobil %s %s",
-    last_night_sleeped_good_format="Minulú noc, ste spali %dh %dmin. Skvelý spánok!",
-    last_night_sleeped_normal_format="Minulú noc, ste spali %dh %dmin.",
-    deep_sleep_format="Hlboká fáza spánku bola %dh %dmin.",
+    activity_activity_format="%s učinil %s %s",
+    last_night_sleeped_good_format="Minulou noc trval spánek %dh %dmin. Skvělý spánek!",
+    last_night_sleeped_normal_format="Minulou noc trval spánek %dh %dmin.",
+    deep_sleep_format="Hluboká fáze spánku trvala %dh %dmin.",
 
-    battery_low_info="Batéria je vybitá, pripojte MiBand na nabíjačku.",
-    battery_very_low_info="Pripojte na nabíjačku",
+    battery_low_info="Baterie je vybitá, připojte MiBand na nabíječku.",
+    battery_very_low_info="Připojte na nabíječku",
 
-    cannot_find_bracelet="Nebolo možné nájsť váš MiBand?",
-    cannot_find_bracelet_info="Uistite sa, že váš MiBand je nabitý a nachádza sa v blízkosti vášho telefónu.",
+    cannot_find_bracelet="Nebylo možné najít váš MiBand?",
+    cannot_find_bracelet_info="Ujistěte se, že váš MiBand je nabitý a nachází se v blízkosti vašeho telefonu.",
  }
